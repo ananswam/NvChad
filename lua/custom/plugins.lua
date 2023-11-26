8f64344f922b11dd389df5a7880126a057b3fcf1
Anandh Swaminathan <ananswam@alum.mit.edu>
2023-11-26 10:49:27 -0800

    Adding lua server (not working for some reason in core) and adding key map, jj for esc, and dg for go back in code navigation (opposite of gd)

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,62 @@
+local plugins = {
+  {
+    "zbirenbaum/copilot.lua",
+    event="InsertEnter",
+    config=function()
+      return require("copilot").setup(require("custom.configs.copilot") )
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      -- local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+    end
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    }
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, _)
+      require("core.utils").load_mappings("dap")
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+        require "plugins.configs.lspconfig"
+        require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "codelldb",
+        "pyright",
+        "lua-language-server",
+      }
+    }
+  }
+}
+return plugins
